cce1aac3bfb6ccde4e0d5bb4023f6b3fbeefdb72
mbentz <mbentz@ufl.edu>
2017-09-28 21:45:03 -0400

    Added event handler to fab

--- portfolio/src/components/fab.js
+++ portfolio/src/components/fab.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 const FAB = () => {
     return (
-        <div>
-            <p>FAB works!</p>
+        <div
+            onClick={doThis} 
+            className="FAB">
+            <p>Contact Me</p>
         </div>
     )
 };
+
+function doThis() {
+    console.log('Fab was clicked');
+}
 
 export default FAB;
