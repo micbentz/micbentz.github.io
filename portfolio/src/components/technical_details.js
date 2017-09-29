76d2091cc0ee5f0530cdc7a5c07ef8b4127448e7
mbentz <mbentz@ufl.edu>
2017-09-28 21:46:43 -0400

    Added list

--- portfolio/src/components/technical_details.js
+++ portfolio/src/components/technical_details.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const TechnicalDetails = () => {
     return (
-        <div>
-            <p>Technical Details works!</p>
+        <div className="TechnicalDetails">
+            <ul className="TechnicalDetails-list">
+                <h2>Programming Experience</h2>
+                <li>Javascript</li>
+                <li>Javascript</li>
+                <li>Javascript</li>
+                <li>Javascript</li>
+                <li>Javascript</li>
+            </ul>
         </div>
     )
 };
