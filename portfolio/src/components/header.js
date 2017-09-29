229683c5eda6f3acf23a44222109e7d83538167b
mbentz <mbentz@ufl.edu>
2017-09-28 21:45:53 -0400

    Repositioned name and title to maintain max width

--- portfolio/src/components/header.js
+++ portfolio/src/components/header.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <p>Header works</p>
+        <div className="Header">
+            <div className="Header-responsive">
+                <h5>Built using React!</h5>
+                <div className="my-info limit-width">
+                    <h1>Michael Bentz</h1>
+                    <h4>UF Alumni</h4>
+                </div>
+            </div>
         </div>
     )
 };
