7ad27bdf24a050fb5464507c76d5bffb96bcaf6f
mbentz <mbentz@ufl.edu>
2017-09-28 21:46:15 -0400

    Added two sections to footer component

--- portfolio/src/components/footer.js
+++ portfolio/src/components/footer.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <p>Footer works</p>
+        <div className="Footer">
+            <div className="Footer-content">
+                <p>Footer works</p>
+            </div>
+            <div className="Footer-copyright">
+                <p>Michael Bentz 2017.</p>
+            </div>
         </div>
     )
 };
